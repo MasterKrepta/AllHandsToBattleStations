668af0f73a6eb267a9ce933e158dcb7034d959a0
Brian <brianj.pajak@gmail.com>
2023-05-21 13:51:02 -0400

    Improvements to lean mechanic

--- Assets/_Scripts/LeanModel.cs
+++ Assets/_Scripts/LeanModel.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LeanModel : MonoBehaviour
 {
+    PlayerControls controls;
     Rigidbody rb;
-    Quaternion initialRotation, targetRotation;
+    Quaternion initialRotation;
     [SerializeField] float leanAmount = 3f;
     [SerializeField] float maxLeanAngle = 25f;
     [SerializeField] float leanTime = 2f;
-    float rotY;
-    float t;
-    float currentLeanTime = 0f;
+
+    [SerializeField] int movementDirection, facingDir, facingRight;
+
+    [SerializeField] float leanX, leanZ;
+    [SerializeField] Vector3 forward;
+    float dot;
+    [SerializeField] Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponentInParent<Rigidbody>();
         initialRotation = transform.localRotation;
+
     }
 
     void Update()
@@ -28,16 +35,31 @@ public class LeanModel : MonoBehaviour
 
     private void HandleLean()
     {
+        forward = transform.forward;
+        facingDir = forward.z >= 0 ? 1 : -1;
+        facingRight = forward.x >= 0 ? 1 : -1;
+        //TODO we need a fix for when the ship is facing left or right. 
+        // if (forward.x > forward.z)
+        // {
+        //     facingDir = forward.z >= 0 ? 1 : -1;
+        // }
+        // else
+        // {
+        //     facingDir = forward.x >= 0 ? 1 : -1;
+        // }
+
+
+        Vector3 velocity = rb.velocity.normalized;
+
+        dot = Vector3.Dot(velocity, forward);
+        movementDirection = dot >= 0 ? 1 : -1;
+
         if (rb.velocity.magnitude > 0.1f)
         {
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-
-            float dot = Vector3.Dot(rb.velocity.normalized, forward);
-            int direction = dot >= 0 ? 1 : -1;
-
             // Calculate the rotation angles based on the velocity and ship orientation
-            float leanX = -rb.velocity.z * direction * leanAmount;
-            float leanZ = rb.velocity.x * direction * leanAmount;
+            leanX = -rb.velocity.z * movementDirection * leanAmount;
+            leanZ = rb.velocity.x * movementDirection * leanAmount;
+
 
             // Check if the ship is moving forwards or backwards and adjust the signs of the lean angles accordingly
             if (dot < 0)
@@ -50,26 +72,17 @@ public class LeanModel : MonoBehaviour
             leanX = Mathf.Clamp(leanX, -maxLeanAngle, maxLeanAngle);
             leanZ = Mathf.Clamp(leanZ, -maxLeanAngle, maxLeanAngle);
 
-            targetRotation = Quaternion.Euler(-leanX, 0, -leanZ);
-            //TODO fix issue where rotation is not applied to initial rotation. 
+            //targetRotation = Quaternion.Euler(-leanX * facingDir, rb.transform.localRotation.y, -leanZ * facingDir);
+            targetRotation = Quaternion.Euler(-leanX * facingDir, rb.transform.localRotation.y, -leanZ * facingDir);
 
             // Slerp the rotation from the current rotation to the target rotation
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * leanAmount);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * leanAmount);
         }
         else
         {
             transform.localRotation = Quaternion.Slerp(transform.localRotation, initialRotation, leanTime * Time.deltaTime);
+            leanX = 0;
+            leanZ = 0;
         }
-    }
-
-    private void ResetLeanTime()
-    {
-        currentLeanTime += Time.deltaTime;
-        if (currentLeanTime > leanTime)
-        {
-            currentLeanTime = leanTime;
-        }
-
-        t = currentLeanTime / leanTime;
     }
 }
